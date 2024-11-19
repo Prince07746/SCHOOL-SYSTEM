2f906669b60470497f7c7eb68ca7b3811abfb799
Matongo prince <davidkitoko13@gmail.com>
2024-11-19 06:58:50 +0300

    change the remote url

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args){
         DBManager db = new DBManager("root","selemani","company");
+        // check integrity
         System.out.println(db.connectDBTechnician());
     }
 
