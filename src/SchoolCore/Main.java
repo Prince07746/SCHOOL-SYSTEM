3d70c47754b510b6544c3384accc9642c8985838
Matongo prince <davidkitoko13@gmail.com>
2024-11-21 13:14:11 +0300

    fixing main

--- src/SchoolCore/Main.java
+++ src/SchoolCore/Main.java
@@ -13,14 +13,19 @@ public class Main {
         db.connectDBTechnician();
         db.getAllDataFromTable("student");
 
-        // check integrity
-        //  System.out.println(db.connectDBTechnician());
+        // check integrity ==> this the most entry point of the all system because it can create a database if not
+        // yet and tables and fix all the small problem that you might have
+        // in upcoming version it will have a lot of functionalities in order to keep the system run
 
-        //  db.deleteDatabase();
+         System.out.println(db.connectDBTechnician());
 
-      // ArrayList<String> myDBs = new ArrayList<>(Arrays.asList("school1","school2","school3"));
+        // this will delete a database if empty name this will delete the actual database if connected
+         db.deleteDatabase();
 
-      // db.microDatabase(myDBs);
+         ArrayList<String> myDBs = new ArrayList<>(Arrays.asList("school1","school2","school3"));
+
+         // this will create multiple database simultaneously  for upcoming school
+         db.microDatabase(myDBs);
 
 
 
